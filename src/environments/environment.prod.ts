d9919a9e3f4796403aaca1a09ff9f8df1736218c
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-05-23 05:24:49 +0700

    fix: add primeng

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fireclipper-f719b',
+    appId: '1:656537213102:web:fd7790559fd8cc3909b53f',
+    storageBucket: 'fireclipper-f719b.appspot.com',
+    apiKey: 'AIzaSyD3gESBsGzCt4VB_h54MqbeGDdG9nCYfVA',
+    authDomain: 'fireclipper-f719b.firebaseapp.com',
+    messagingSenderId: '656537213102',
+    measurementId: 'G-Y0CSNWP670',
+  },
   production: true
 };
