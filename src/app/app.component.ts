96c56837df6d9925d63a038bbed527e08f4b5cb9
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-05-23 09:27:19 +0700

    feat: auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fireclipper-ng';
+  items: MenuItem[] = [
+    {
+      label: 'Home',
+      routerLink: '/',
+    },
+    // {
+    //   label: 'Edit',
+    //   icon: 'pi pi-fw pi-pencil',
+    //   items: [
+    //     { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+    //     { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+    //   ]
+    // }
+  ]
+  signedIn = false;
+
+  constructor(
+    private auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {
+    auth.authState.subscribe(x => this.signedIn = x != null);
+  }
+
+  async signIn() {
+    const response = await this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    if (response) {
+      if (response.additionalUserInfo?.isNewUser) {
+        this.firestore
+          .collection('users')
+          .doc(response.user?.uid)
+          .set({
+            displayName: response.user?.displayName,
+            email: response.user?.email
+          });
+      }
+    }
+  }
+
+  async signOut() {
+    this.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,31 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { HomeComponent } from './components/home/home.component';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    MenubarModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
